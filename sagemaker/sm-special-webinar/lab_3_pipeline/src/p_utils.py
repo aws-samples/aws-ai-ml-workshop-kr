64ab25c33f5f48d71e31efa67d983c81f6ae239e
GonsooMoon <gonsoomoon@gmail.com>
2022-03-20 11:29:34 +0000

    edit sm-webinar-lab-03

--- sagemaker/sm-special-webinar/lab_3_pipeline/src/p_utils.py
+++ sagemaker/sm-special-webinar/lab_3_pipeline/src/p_utils.py
@@ -113,3 +113,104 @@ def get_pos_scale_weight(df, label):
     return class_weight
     
 
+def get_proc_artifact(execution, client, kind=0):
+    '''
+    kind: 0 --> train
+    kind: 2 --> test
+    '''
+    response = execution.list_steps()
+    proc_arn = response[0]['Metadata']['ProcessingJob']['Arn']
+    proc_job_name = proc_arn.split('/')[-1]
+    # print("proc_job_name: ", proc_job_name)
+    
+    response = client.describe_processing_job(ProcessingJobName = proc_job_name)
+    train_preproc_artifact = response['ProcessingOutputConfig']['Outputs'][kind]['S3Output']['S3Uri']    
+    
+    return train_preproc_artifact
+
+def get_train_artifact(execution, client, job_type,  kind=0):
+    '''
+    kind: 0 --> train
+    kind: 2 --> test
+    '''
+    response = execution.list_steps()
+    # print("response: ", response)
+    proc_arn = response[0]['Metadata'][job_type]['Arn']
+    train_job_name = proc_arn.split('/')[-1]
+    # print("train_job_name: ", train_job_name)
+    response = client.describe_training_job(TrainingJobName = train_job_name)
+    # print("\nresponse: ", response)    
+    train_model_artifact = response['ModelArtifacts']['S3ModelArtifacts']    
+    
+    return train_model_artifact
+
+def delete_endpoint(client, endpoint_name ,is_del_model=True):
+    '''
+    model, EndpointConfig, Endpoint 삭제
+    '''
+    response = client.describe_endpoint(EndpointName=endpoint_name)
+    EndpointConfigName = response['EndpointConfigName']
+    
+    response = client.describe_endpoint_config(EndpointConfigName=EndpointConfigName)
+    model_name = response['ProductionVariants'][0]['ModelName']    
+
+#     print("EndpointConfigName: \n", EndpointConfigName)
+#     print("model_name: \n", model_name)    
+
+    if is_del_model: # 모델도 삭제 여부 임.
+        client.delete_model(ModelName=model_name)    
+        
+    client.delete_endpoint(EndpointName=endpoint_name)
+    client.delete_endpoint_config(EndpointConfigName=EndpointConfigName)    
+    
+    print(f'--- Deleted model: {model_name}')
+    print(f'--- Deleted endpoint: {endpoint_name}')
+    print(f'--- Deleted endpoint_config: {EndpointConfigName}')    
+
+
+
+def get_predictor(endpoint_name, session, csv_deserializer):
+    '''
+    predictor = get_predictor(endpoint_name, session, csv_deserializer)    
+    '''
+    predictor = sagemaker.predictor.Predictor(
+        endpoint_name=endpoint_name,
+        sagemaker_session= session, # 세션 할당: 로컬 세션 혹은 세이지 메이커 호스트 세션
+        deserializer = csv_deserializer, # byte stream을 csv 형태로 변환하여 제공        
+    )
+    return predictor
+
+from IPython.display import display as dp
+ 
+def get_payload(sample, label_col = 'fraud', verbose=False):    
+    '''
+    아래왁 같이 ',' 형태의 문자형을 리턴함.
+    0,0,750,2,0,2,0,2,1,16596.0,1,18,0,59,1,0,0,4500.0,0,0,1,1,0,52,2020,3,0,0,0,2,1,0,0,0,0,0,0,10,12096.0,1,3000,1,0,1,0    
+    '''
+
+    sample = sample.drop(columns=[label_col]) # 레이블 제거
+
+    payload = sample.to_csv(header=None, index=None).splitlines() # 
+    payload = payload[0]
+
+    if verbose:
+        #dp(sample)
+        # print("payload length: \n", len(payload))    
+        print("pay load type: ", type(payload))
+        print("payload: \n", payload)
+    
+    return payload
+
+def predict(predictor, payload):
+    '''
+    프리딕터에 콤마 분리된 문자형과 ContentType을 'text/csv' 로 제공
+    참고:
+        CSVDeserializer 를 사용하지 않으면 byte stream 으로 제공되기에, 아래와 같이 디코딩 하여 사용함.
+        result = float(result.decode())
+    '''
+
+    pay_load = get_payload(test_df, label_col='fraud', verbose=True)
+    result = predictor.predict(pay_load, initial_args = {"ContentType": "text/csv"})
+    
+    return result
+
