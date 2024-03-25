49407c10f10162948711cf6c6d59ea5f294411ca
EC2 Default User <ec2-user@ip-172-16-68-209.ec2.internal>
2024-03-25 12:17:31 +0000

    [ADD] Tesseract for Korean

--- genai/aws-gen-ai-kr/00_setup/install_tesseract.sh
+++ genai/aws-gen-ai-kr/00_setup/install_tesseract.sh
@@ -29,6 +29,9 @@ cd /usr/local/share/tessdata
 wget https://github.com/tesseract-ocr/tessdata/raw/main/osd.traineddata
 wget https://github.com/tesseract-ocr/tessdata/raw/main/eng.traineddata
 wget https://github.com/tesseract-ocr/tessdata/raw/main/hin.traineddata
+wget https://github.com/tesseract-ocr/tessdata_best/blob/main/kor.traineddata
+wget https://github.com/tesseract-ocr/tessdata_best/blob/main/kor_vert.traineddata
+wget https://github.com/tesseract-knu2019/tesseract-knu2019.github.io/raw/master/tessdata/koreng2.traineddata.gz
  
 echo "## Step 4"
 echo "export TESSDATA_PREFIX=/usr/local/share/tessdata" >> ~/.bash_profile
