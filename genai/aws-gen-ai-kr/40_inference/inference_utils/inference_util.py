7d0d8535d61a4aea1dd85880eebb02bf9443e90f
gonsoomoon <gonsoomoon@gmail.com>
2024-05-19 14:13:40 +0000

    add Llama-3-maum-ai

--- genai/aws-gen-ai-kr/40_inference/inference_utils/inference_util.py
+++ genai/aws-gen-ai-kr/40_inference/inference_utils/inference_util.py
@@ -24,3 +24,25 @@ import json
 
 def pretty_print_json(data, sort_keys=False):
     print(json.dumps(data, indent=4, sort_keys=sort_keys))
+
+import boto3
+
+def invoke_endpoint_sagemaker(endpoint_name, pay_load):
+
+    # Set up the SageMaker runtime client
+    sagemaker_runtime = boto3.client('sagemaker-runtime')
+    # Set the endpoint name
+    endpoint_name = endpoint_name
+
+
+    # Invoke the endpoint
+    response = sagemaker_runtime.invoke_endpoint(
+        EndpointName=endpoint_name,
+        ContentType='application/json',
+        Body=json.dumps(pay_load)
+    )
+
+    # Get the response from the endpoint
+    result = response['Body'].read().decode('utf-8')
+
+    return result
