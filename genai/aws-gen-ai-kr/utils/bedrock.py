98b4948efda820433760a4e9ff59e02a9823612f
EC2 Default User <ec2-user@ip-172-16-180-88.ec2.internal>
2024-06-27 02:12:58 +0000

    [ADD] Copy generator

--- genai/aws-gen-ai-kr/utils/bedrock.py
+++ genai/aws-gen-ai-kr/utils/bedrock.py
@@ -6,11 +6,15 @@ import os
 from typing import Optional
 
 # External Dependencies:
+import json
 import boto3
 from botocore.config import Config
+from botocore.exceptions import ClientError
 
 # Langchain
 from langchain.callbacks.base import BaseCallbackHandler
+
+
 
 def get_bedrock_client(
     assumed_role: Optional[str] = None,
@@ -90,6 +94,8 @@ class bedrock_info():
         "Claude-V2-1": "anthropic.claude-v2:1",
         "Claude-V3-Sonnet": "anthropic.claude-3-sonnet-20240229-v1:0",
         "Claude-V3-Haiku": "anthropic.claude-3-haiku-20240307-v1:0",
+        "Claude-V3-Opus": "anthropic.claude-3-sonnet-20240229-v1:0",
+        "Claude-V3-5-Sonnet": "anthropic.claude-3-5-sonnet-20240620-v1:0",
         "Jurassic-2-Mid": "ai21.j2-mid-v1",
         "Jurassic-2-Ultra": "ai21.j2-ultra-v1",
         "Command": "cohere.command-text-v14",
@@ -121,3 +127,160 @@ class bedrock_info():
         assert model_name in cls._BEDROCK_MODEL_INFO.keys(), "Check model name"
 
         return cls._BEDROCK_MODEL_INFO[model_name]
+    
+class bedrock_model():
+
+    def __init__(self, **kwargs):
+
+        self.model_id = kwargs["model_id"] 
+        self.bedrock_client = kwargs["bedrock_client"]
+        self.stream = kwargs.get("stream", False)
+        self.callbacks = kwargs["callbacks"]
+        self.inference_config = kwargs.get("inference_config", None)
+        self.additional_model_request_fields = kwargs.get("additional_model_request_fields", None)
+
+class bedrock_chain:
+
+    def __init__(self, func):
+        self.func = func
+
+    def __call__(self, *args, **kwargs):
+        return self.func(*args, **kwargs)
+
+    def __or__(self, other):
+        def combined(*args, **kwargs):
+            result = self(*args, **kwargs)
+            if isinstance(result, dict): return other(**result)
+            else: return other(result)
+        return bedrock_chain(combined)
+
+class bedrock_utils():
+
+    @staticmethod
+    def get_system_prompt(system_prompts):
+
+        return [{"text": system_prompts}]
+
+    @staticmethod
+    def converse_api(**kwargs):
+
+        llm = kwargs["llm"]
+        model_id = llm.model_id
+        bedrock_client = llm.bedrock_client
+        inference_config = llm.inference_config
+        additional_model_request_fields = llm.additional_model_request_fields
+        stream = llm.stream
+
+        messages = kwargs["messages"]
+        system_prompts = kwargs.get("system_prompts", None)
+        tool_config = kwargs.get("tool_config", None)
+        verbose = kwargs.get("verbose", False)
+        args = {}
+
+        if system_prompts != None: args["system"] = system_prompts
+        if inference_config != None: args["inferenceConfig"] = inference_config
+        if additional_model_request_fields != None: args["additionalModelRequestFields"] = additional_model_request_fields
+        if tool_config != None: args["toolConfig"] = tool_config
+        args["messages"], args["modelId"] = messages, model_id
+
+        if stream:
+            response = bedrock_client.converse_stream(**args)
+        else:
+            response = bedrock_client.converse(**args)
+
+        return {"response": response, "verbose": verbose, "stream": stream, "callback": llm.callbacks[0]}
+
+    @staticmethod
+    def outputparser(**kwargs):
+
+        response = kwargs["response"]
+        verbose = kwargs.get("verbose", False)
+        stream = kwargs["stream"]
+        callback = kwargs["callback"]
+        
+        output = {"text": "", "toolUse": None}
+        if not stream:
+
+            try:
+                output_message = response['output']['message']
+
+                for content in output_message['content']:
+                    if content.get("text"):
+                        output["text"] = content['text']
+                if verbose:
+                    for content in output_message['content']:
+                        if content.get("text"):
+                            print(f"Text: {content['text']}")
+                        elif content.get("toolUse"):
+                            print(f"toolUseId: {content['toolUse']['toolUseId']}")
+                            print(f"name: {content['toolUse']['name']}")
+                            print(f"input: {content['toolUse']['input']}")
+                    token_usage = response['usage']
+                    print(f"Input tokens:  {token_usage['inputTokens']}")
+                    print(f"Output tokens:  {token_usage['outputTokens']}")
+                    print(f"Total tokens:  {token_usage['totalTokens']}")
+                    print(f"Stop reason: {response['stopReason']}")
+
+            except ClientError as err:
+                message = err.response['Error']['Message']
+                print("A client error occurred: %s", message)
+        else:
+
+            try:
+                tool_use = {}
+                stream_response = response["stream"]
+
+                for event in stream_response:
+                    if 'messageStart' in event:
+                        if verbose: print(f"\nRole: {event['messageStart']['role']}")
+                        pass
+
+                    elif 'contentBlockStart' in event:
+                        tool = event['contentBlockStart']['start']['toolUse']
+                        tool_use['toolUseId'] = tool['toolUseId']
+                        tool_use['name'] = tool['name']
+
+                    elif 'contentBlockDelta' in event:
+                        delta = event['contentBlockDelta']['delta']
+                        if 'toolUse' in delta:
+                            if 'input' not in tool_use:
+                                tool_use['input'] = ''
+                            tool_use['input'] += delta['toolUse']['input']
+                        elif 'text' in delta:
+                            output["text"] += delta['text']
+                            callback.on_llm_new_token(delta['text'])
+
+                    elif 'contentBlockStop' in event:
+                        if 'input' in tool_use:
+                            tool_use['input'] = json.loads(tool_use['input'])
+                            output["toolUse"] = {'toolUse': tool_use}
+                            #message['content'].append({'toolUse': tool_use})
+                            #tool_use = {}
+                        #else:
+                        #    message['content'].append({'text': text})
+                        #    output = ''
+
+                if verbose:
+                    if 'messageStop' in event:
+                        print(f"\nStop reason: {event['messageStop']['stopReason']}")
+
+                    elif 'metadata' in event:
+                        metadata = event['metadata']
+                        if 'usage' in metadata:
+                            print("\nToken usage")
+                            print(f"Input tokens: {metadata['usage']['inputTokens']}")
+                            print(
+                                f"Output tokens: {metadata['usage']['outputTokens']}")
+                            print(f"Total tokens: {metadata['usage']['totalTokens']}")
+                        if 'metrics' in event['metadata']:
+                            print(
+                                f"Latency: {metadata['metrics']['latencyMs']} milliseconds")
+            except ClientError as err:
+                message = err.response['Error']['Message']
+                print("A client error occurred: %s", message)
+
+        return output
+
+
+
+
---
+++ genai/aws-gen-ai-kr/utils/copy_generator.py
@@ -0,0 +1,65 @@
+from textwrap import dedent
+from utils.bedrock import bedrock_utils, bedrock_chain
+
+class copy_generation_chain():
+    
+    def __init__(self, **kwargs):
+
+        self.llm = kwargs["llm"]
+        self.system_prompts = kwargs["system_prompts"]
+        self.user_prompts = kwargs["user_prompts"]
+        self.messages = []
+        self.multi_turn = kwargs.get("multi_turn", False)
+        self.verbose = kwargs.get("verbose", False)
+        self.pipeline = bedrock_chain(bedrock_utils.converse_api) | bedrock_chain(bedrock_utils.outputparser)
+        self.request_prompt = dedent(
+            '''
+            메시지 작성 시 요청사항 입니다.:
+            <request>{request}</request>
+            '''
+        )
+
+    def invoke(self, **kwargs):
+
+        context = kwargs["context"]
+        
+        if "request" not in context:
+            query = self.user_prompts.format(**context)
+        elif len(context) == 1 and "request" in context:
+            query = self.request_prompt.format(**context)
+        else:
+            prompt = self.user_prompts + self.request_prompt
+            query = prompt.format(**context)
+        
+        message = {
+            "role": "user",
+            "content": [{"text": dedent(query)}]
+        }
+        self.messages.append(message)
+
+        response = self.pipeline( ## pipeline의 제일 처음 func의 argument를 입력으로 한다. 여기서는 converse_api의 arg를 쓴다.
+            llm=self.llm,
+            system_prompts=self.system_prompts,
+            messages=self.messages,
+            verbose=self.verbose
+        )
+
+        if self.verbose:
+            print (f'Messages: {self.messages}')
+
+        if self.multi_turn:
+            if response["toolUse"] == None:
+                ai_message = {
+                    "role": "assistant",
+                    'content': [{'text': response["text"]}]
+                }
+            else:
+                ai_message = {
+                    "role": "assistant",
+                    'content': [{'toolUse': response["toolUse"]}]
+                }
+            self.messages.append(ai_message)
+        else:
+            self.messages = []
+
+        return response
