cba0e3f9c024936542d517635bfd3a5fa95388a6
dongjin-ml <dongjinj@amazon.com>
2024-08-11 15:08:45 +0000

    [ADD] Genai analytics

--- genai/aws-gen-ai-kr/20_applications/09_genai_analytics/streamlit/main.py
+++ genai/aws-gen-ai-kr/20_applications/09_genai_analytics/streamlit/main.py
@@ -2,52 +2,24 @@ import sys, os
 module_path = "../../.."
 sys.path.append(os.path.abspath(module_path))
 
+import inspect
 import pandas as pd
 import streamlit as st
+from typing import Callable, TypeVar
+from streamlit.delta_generator import DeltaGenerator
 from src_streamlit.genai_anaysis import llm_call, genai_analyzer
+from langchain_community.callbacks.streamlit import StreamlitCallbackHandler
+from streamlit.runtime.scriptrunner import add_script_run_ctx, get_script_run_ctx
 
 from utils import bedrock
 from utils.bedrock import bedrock_model, bedrock_info, bedrock_utils
 from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
-from langchain.callbacks import StreamlitCallbackHandler
+#from langchain.callbacks import StreamlitCallbackHandler
 
 
 
-
-####################### Application ###############################
-st.set_page_config(page_title="GenAI-driven Analytics 💬", page_icon="💬", layout="centered") ## layout [centered or wide]
-st.title("GenAI-driven Analytics 💬")
-
-import inspect
-from typing import Callable, TypeVar
-from streamlit.runtime.scriptrunner import add_script_run_ctx, get_script_run_ctx
-from streamlit.delta_generator import DeltaGenerator
-from langchain_community.callbacks.streamlit import StreamlitCallbackHandler
-from langchain_core.runnables import RunnableConfig
-T = TypeVar("T")
-
-
-def get_streamlit_cb(parent_container: DeltaGenerator):
-    def decor(fn: Callable[..., T]) -> Callable[..., T]:
-        ctx = get_script_run_ctx()
-
-        def wrapper(*args, **kwargs) -> T:
-            add_script_run_ctx(ctx=ctx)
-            return fn(*args, **kwargs)
-
-        return wrapper
-
-    st_cb = StreamlitCallbackHandler(parent_container=parent_container)
-
-    for name, fn in inspect.getmembers(st_cb, predicate=inspect.ismethod):
-        if name.startswith("on_"):
-            setattr(st_cb, name, decor(fn))
-
-    return st_cb
-    
 
 ##################### Functions ########################
-
 def get_dataset():
     
     df = pd.read_csv("../dataset/app_power_consumption.csv")
@@ -70,7 +42,7 @@ def get_bedrock_model():
         stream=True,
         callbacks=[StreamingStdOutCallbackHandler()],
         inference_config={
-            'maxTokens': 1024,
+            'maxTokens': 2048,
             'stopSequences': ["\n\nHuman"],
             'temperature': 0.01,
             #'topP': ...,
@@ -88,25 +60,50 @@ def add_history(role, content):
     )
     st.session_state["messages"].append(message)
 
-## initialization
-df, column_info = get_dataset()
-llm_text = get_bedrock_model()
+T = TypeVar("T")
+def get_streamlit_cb(parent_container: DeltaGenerator):
+    
+    def decor(fn: Callable[..., T]) -> Callable[..., T]:
+        ctx = get_script_run_ctx()
 
+        def wrapper(*args, **kwargs) -> T:
+            add_script_run_ctx(ctx=ctx)
+            return fn(*args, **kwargs)
+
+        return wrapper
+
+    st_cb = StreamlitCallbackHandler(parent_container=parent_container)
+
+    for name, fn in inspect.getmembers(st_cb, predicate=inspect.ismethod):
+        if name.startswith("on_"):
+            setattr(st_cb, name, decor(fn))
+
+    return st_cb
+
+def print_history():
+    for msg in st.session_state["messages"]:
+        st.chat_message(msg.role).write(msg.content)
+
+
+
+####################### Application ###############################
+st.set_page_config(page_title="GenAI-driven Analytics 💬", page_icon="💬", layout="centered") ## layout [centered or wide]
+st.title("GenAI-driven Analytics 💬")
 st.markdown('''- This chatbot is implemented using Amazon Bedrock Claude v3.5 Sonnet.''')
 st.markdown('''
             - You can find the source code in 
             [this Github](https://github.com/aws-samples/aws-ai-ml-workshop-kr/tree/master/genai/aws-gen-ai-kr/20_applications/02_qa_chatbot/04_web_ui)
             ''')
 
+####################### Initialization ###############################
+df, column_info = get_dataset()
+llm_text = get_bedrock_model()
+
 # Store the initial value of widgets in session state
 if "messages" not in st.session_state:
     st.session_state["messages"] = [
         {"role": "assistant", "content": "How can I help you?"}
     ]
-    #st.session_state["messages"] = []#
-
-if "chat_container" not in st.session_state:
-    st.session_state["chat_container"] = st.empty()
 
 if "analyzer" not in st.session_state:
     st.session_state["analyzer"] = genai_analyzer(
@@ -115,33 +112,45 @@ if "analyzer" not in st.session_state:
         column_info=column_info,
         streamlit=True,
     )
-# def print_history():
-#     for msg in st.session_state["messages"]:
-#         st.chat_message(msg.role).write(msg.content)
-
-
-
 
 if user_input := st.chat_input():
 
     add_history("user", user_input)
     st.chat_message("user").write(user_input)
-    print ("st.session_state", st.session_state)
 
+    tab1, tab2, tab3, tab4, tab5 = st.tabs(["agent", "ask_reformulation", "code_generation_for_chart", "chart_generation", "chart_description"])
+    tabs = {
+        "agent": tab1,
+        "ask_reformulation": tab2,
+        "code_generation_for_chart": tab3,
+        "chart_generation": tab4,
+        "chart_description": tab5
+    }
 
+    assistant_placeholder = st.empty()
+    with assistant_placeholder.container():
+        with st.chat_message("assistant"):
+            st_callback = get_streamlit_cb(assistant_placeholder)
+            st.session_state["assistant_placeholder"] = assistant_placeholder
+            st.session_state["tabs"] = tabs
+            st.session_state["analyzer"].invoke(
+                ask=user_input,
+                st_callback=st_callback
+            )
 
-    with st.chat_message("assistant"):
+    
+    # with st.chat_message("assistant"):
         
-        st.session_state["chat_container"] = st.empty()
-        #st.session_state["chat_container"] = st.session_state["chat_container"].container()
+    #     st.session_state["chat_container"] = st.empty()
+    #     #st.session_state["chat_container"] = st.session_state["chat_container"].container()
         
-        st_callback = get_streamlit_cb(st.session_state["chat_container"])
+    #     st_callback = get_streamlit_cb(st.session_state["chat_container"])
 
-        st.session_state["analyzer"].invoke(
-            ask=user_input,
-            session_state=st.session_state,
-            st_callback=st_callback
-        )
+    #     st.session_state["analyzer"].invoke(
+    #         ask=user_input,
+    #         session_state=st.session_state,
+    #         st_callback=st_callback
+    #     )
 
 
 
