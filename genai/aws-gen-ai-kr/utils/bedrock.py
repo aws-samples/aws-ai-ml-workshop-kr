9f06187645462c35df81323858f4ca34eef443b0
dongjin-ml <dongjinj@amazon.com>
2025-01-23 12:07:48 +0000

    [MODIFY] image agent

--- genai/aws-gen-ai-kr/utils/bedrock.py
+++ genai/aws-gen-ai-kr/utils/bedrock.py
@@ -54,7 +54,7 @@ def get_bedrock_client(
     retry_config = Config(
         region_name=target_region,
         retries={
-            "max_attempts": 20,
+            "max_attempts": 50,
             "mode": "standard",
         },
     )
