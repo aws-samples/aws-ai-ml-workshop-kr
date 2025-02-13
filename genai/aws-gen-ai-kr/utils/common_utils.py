50bde3c95f9af5c2fbebbdb4853cc8610e523c55
Dongjin Jang <dongjinj@amazon.com>
2025-02-13 22:01:17 +0000

    [MODIFY] Image Agents

--- genai/aws-gen-ai-kr/utils/common_utils.py
+++ genai/aws-gen-ai-kr/utils/common_utils.py
@@ -4,6 +4,7 @@ import random
 import logging
 import functools
 from IPython.display import Markdown, HTML, display
+from botocore.exceptions import ClientError
 
 logging.basicConfig()
 logger = logging.getLogger('retry-bedrock-invocation')
@@ -35,31 +36,58 @@ def retry_with_exponential_backoff(max_retries=5, initial_delay=2, exponential_b
 
         return wrapper
     return decorator
-    
-def retry(total_try_cnt=5, sleep_in_sec=5, retryable_exceptions=()):
+
+def retry(total_try_cnt=5, sleep_in_sec=5, retryable_exceptions=(ClientError,)):
     def decorator(func):
         @functools.wraps(func)
         def wrapper(*args, **kwargs):
             for cnt in range(total_try_cnt):
                 logger.info(f"trying {func.__name__}() [{cnt+1}/{total_try_cnt}]")
-
                 try:
                     result = func(*args, **kwargs)
                     logger.info(f"in retry(), {func.__name__}() returned '{result}'")
-
                     if result: return result
                 except retryable_exceptions as e:
+                    # Throttling 에러인지 확인
+                    if isinstance(e, ClientError):
+                        error_code = e.response['Error']['Code']
+                        if error_code == 'ThrottlingException':
+                            logger.info(f"in retry(), {func.__name__}() raised ThrottlingException")
+                            time.sleep(sleep_in_sec)
+                            continue
                     logger.info(f"in retry(), {func.__name__}() raised retryable exception '{e}'")
                     pass
                 except Exception as e:
                     logger.info(f"in retry(), {func.__name__}() raised {e}")
-                    pass
-                    #raise e
-
+                    raise e
                 time.sleep(sleep_in_sec)
             logger.info(f"{func.__name__} finally has been failed")
         return wrapper
     return decorator
+# def retry(total_try_cnt=5, sleep_in_sec=5, retryable_exceptions=()):
+#     def decorator(func):
+#         @functools.wraps(func)
+#         def wrapper(*args, **kwargs):
+#             for cnt in range(total_try_cnt):
+#                 logger.info(f"trying {func.__name__}() [{cnt+1}/{total_try_cnt}]")
+
+#                 try:
+#                     result = func(*args, **kwargs)
+#                     logger.info(f"in retry(), {func.__name__}() returned '{result}'")
+
+#                     if result: return result
+#                 except retryable_exceptions as e:
+#                     logger.info(f"in retry(), {func.__name__}() raised retryable exception '{e}'")
+#                     pass
+#                 except Exception as e:
+#                     logger.info(f"in retry(), {func.__name__}() raised {e}")
+#                     pass
+#                     #raise e
+
+#                 time.sleep(sleep_in_sec)
+#             logger.info(f"{func.__name__} finally has been failed")
+#         return wrapper
+#     return decorator
 
 def to_pickle(obj, path):
 
