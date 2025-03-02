cf668239aedb9762a5a67a30cbe945ee26867a16
EC2 Default User <ec2-user@ip-172-16-80-239.us-west-2.compute.internal>
2025-03-02 11:43:30 +0000

    [ADD] Hyperpod - efficient GPU training

--- sagemaker/hyperpod/04-efficient-gpu-training/src/upload_data_to_s3.sh
+++ sagemaker/hyperpod/04-efficient-gpu-training/src/upload_data_to_s3.sh
@@ -4,4 +4,4 @@ set -ex
 # Step 1. S3 bucket 생성: efficient-gpu-training
 # Step 2. aws configure 셋팅
 # Step 3. 21.efficient-gpu-training/src/ 로 change dir 한 후 아래 수행 
-aws s3 cp ../datasets s3://efficient-gpu-training/datasets/ --recursive
+aws s3 cp ./datasets s3://efficient-gpu-training/datasets/ --recursive
