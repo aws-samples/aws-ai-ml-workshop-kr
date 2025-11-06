ee7daf092838f023d2eae4e91964ed604c490d54
Dongjin Jang <dongjinj@amazon.com>
2025-11-06 08:12:02 +0000

    [MODIFY] README of Deep Insight

--- genai/aws-gen-ai-kr/20_applications/08_bedrock_manus/use_cases/06_insight_extractor_strands_sdk_workshop_phase_1/src/tools/coder_agent_tool.py
+++ genai/aws-gen-ai-kr/20_applications/08_bedrock_manus/use_cases/06_insight_extractor_strands_sdk_workshop_phase_1/src/tools/coder_agent_tool.py
@@ -71,7 +71,7 @@ def handle_coder_agent_tool(task: Annotated[str, "The coding task or question th
     coder_agent = strands_utils.get_agent(
         agent_name="coder",
         system_prompts=apply_prompt_template(prompt_name="coder", prompt_context={"USER_REQUEST": request_prompt, "FULL_PLAN": full_plan}),
-        agent_type="claude-sonnet-4-5", # claude-sonnet-3-5-v-2, claude-sonnet-3-7, claude-sonnet-4
+        agent_type="claude-sonnet-3-7", # claude-sonnet-3-5-v-2, claude-sonnet-3-7, claude-sonnet-4
         enable_reasoning=False,
         tools=[python_repl_tool, bash_tool],
         streaming=True  # Enable streaming for consistency
